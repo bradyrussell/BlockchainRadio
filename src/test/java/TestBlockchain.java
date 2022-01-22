4deb903b7d0ad1b963a6bf5694f8b3f67fafba2b
BradyRussell <31458670+bradyrussell@users.noreply.github.com>
2022-01-22 15:24:24 -0600

    fix tests for ci cd

--- src/test/java/TestBlockchain.java
+++ src/test/java/TestBlockchain.java
@@ -72,7 +72,7 @@ public class TestBlockchain {
         bs.close();
     }
 
-    @Test// @Disabled
+    @Test @Disabled
     void TestBlockchain() throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, IOException, SignatureException, InvalidKeyException {
         BlockchainStorage bs = new BlockchainStorageFlatfile("blockchaindb");
         bs.open();
@@ -90,7 +90,7 @@ public class TestBlockchain {
                 .calculateHashPreviousBlock(bs)
                 .calculateBlockHeight(bs)
                 .calculateDifficultyTarget(bs)
-                .setAudio(Files.readAllBytes(Path.of("C:\\Users\\brady\\Desktop\\ttfaf.ogg")), keyPair);
+                .setAudio(Files.readAllBytes(Path.of("whitenoise.ogg")), keyPair);
 
         System.out.println("Begin mining at difficulty "+candidate.getBlockHeader().DifficultyTarget);
 
